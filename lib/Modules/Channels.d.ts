7131e5aa6d58678975b67aa26e56a8da14942d38
Philip Claren <philip.claren@googlemail.com>
2019-07-31 13:54:26 +0200

    build

--- lib/Modules/Channels.d.ts
+++ lib/Modules/Channels.d.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from './CommonModule';
 export declare class Channels extends CommonModule {
     protected entityType: string;
+    fetchByKey(key: string): Promise<any>;
 }
--- lib/Modules/Products.d.ts
+++ lib/Modules/Products.d.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from './CommonModule';
 import { PagedQueryResult, Product } from '../types';
+import { CommonModule } from './CommonModule';
 export declare class Products extends CommonModule {
     protected entityType: string;
     fetchAllByAttribute(name: string, value: string, page?: number, perPage?: number): Promise<PagedQueryResult<Product>>;
