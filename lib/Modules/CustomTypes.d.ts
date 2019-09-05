ccb6f581143a390e45a4e7add08a150b54a222af
Philip Claren <philip.claren@googlemail.com>
2019-09-05 15:49:43 +0200

    removed redundant methods from CustomTypes class

--- lib/Modules/CustomTypes.d.ts
+++ lib/Modules/CustomTypes.d.ts
@@ -1,7 +1,5 @@
-import { CustomType, CustomTypeDraft, UpdateAction } from '../types';
+import { CustomType, CustomTypeDraft } from '../types';
 import { CommonModule } from './CommonModule';
 export declare class CustomTypes extends CommonModule<CustomType, CustomTypeDraft> {
     protected entityType: string;
-    create(customTypeDraft: CustomTypeDraft): Promise<CustomType>;
-    updateByKey(key: string, actions: UpdateAction[]): Promise<CustomType>;
 }
--- src/Modules/CustomTypes.ts
+++ src/Modules/CustomTypes.ts
@@ -3,39 +3,4 @@ import { CommonModule } from './CommonModule';
 
 export class CustomTypes extends CommonModule<CustomType, CustomTypeDraft>{
   protected entityType = 'types';
-
-  public async create(customTypeDraft: CustomTypeDraft): Promise<CustomType> {
-    const postRequest = {
-      uri: this.request.types.build(),
-      method: 'POST',
-      headers: this.headers,
-      body: customTypeDraft,
-    };
-
-    return (
-      this.client
-        .execute(postRequest)
-        .then(response => response.body)
-    );
-  }
-
-  public async updateByKey(key: string, actions: UpdateAction[]): Promise<CustomType> {
-    const customType = await this.resolveKeyAndVersion(key, this.fetchByKey.bind(this));
-
-    const postRequest = {
-      uri: this.request.types.byKey(key).build(),
-      method: 'POST',
-      headers: this.headers,
-      body: {
-        version: customType.version,
-        actions
-      },
-    };
-
-    return (
-      this.client
-        .execute(postRequest)
-        .then(response => response.body)
-    );
-  }
 }
