7cbc5c833bb4235bfd1562c32a32d32e71cdcea7
Philip Claren <philip@claren.io>
2020-04-21 16:24:18 +0200

    build

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -165,7 +165,8 @@ export interface DiscountedPrice {
 export interface Money {
     type: string;
     currencyCode: string;
-    centAmount: number;
+    centAmount?: number;
+    preciseAmount?: number;
     fractionDigits: number;
 }
 export interface CustomFields {
