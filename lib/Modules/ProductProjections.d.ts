1500af9e50c9560352e074460dcc7af2e6a64fde
Philip Claren <philip@claren.io>
2021-05-06 08:55:43 +0200

    build

--- lib/Modules/ProductProjections.d.ts
+++ lib/Modules/ProductProjections.d.ts
@@ -6,5 +6,10 @@ export declare class ProductProjections extends BaseModule {
         key: string;
         value: string;
     }>): Promise<PagedQueryResult<any>>;
+    suggest(searchTerm: string, locale: string, filter?: Array<{
+        key: string;
+        value: string;
+    }>): Promise<PagedQueryResult<any>>;
+    private searchLike;
     getAllPossibleValuesForAttribute(attributeName: string): Promise<string[]>;
 }
