a69bce8ce859931dbdcbb35379da33be8e89da99
Philip Claren <philip@claren.io>
2022-01-05 10:21:28 +0100

    updated commercetools packages

--- src/Commeretools.ts
+++ src/Commeretools.ts
@@ -5,7 +5,7 @@ import { createAuthMiddlewareForClientCredentialsFlow } from '@commercetools/sdk
 import { createHttpMiddleware } from '@commercetools/sdk-middleware-http';
 import { createQueueMiddleware } from '@commercetools/sdk-middleware-queue';
 import * as modules from './Modules/index';
-import { CommercetoolsConfig } from './types'
+import { CommercetoolsConfig } from './types';
 
 export class Commercetools {
   public get carts(): modules.Carts { return this.initModule('Carts') }
